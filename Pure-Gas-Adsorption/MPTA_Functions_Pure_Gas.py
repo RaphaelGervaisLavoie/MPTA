1b9ed3a9e4f4f5578a9db17354dbe2ce9aaee748
Raphaël Gervais Lavoie <raphael.gervaislavoie@uqtr.ca>
2018-02-22 17:28:58 -0500

    Update MPTA_Functions_Pure_Gas.py

--- Pure-Gas-Adsorption/MPTA_Functions_Pure_Gas.py
+++ Pure-Gas-Adsorption/MPTA_Functions_Pure_Gas.py
@@ -9,10 +9,10 @@ This file provide the functions used in the implementation of the MPTA for pure 
  License: MIT
  Author: Raphaël Gervais Lavoie (raphael.gervaislavoie@uqtr.ca)
  
+ 
  If you find this code usefull, please cite the folowing reference:
 
-
-> "Numerical implementation of the multicomponent potential theory of adsorption in Python using the NIST Refprop", 
+"Numerical implementation of the multicomponent potential theory of adsorption in Python using the NIST Refprop", 
  Raphaël Gervais Lavoie, Mathieu Ouellet, Jean Hamelin and Pierre Bénard,  
  Commun. Comput. Phys., Vol. 23, No. 5, 2018, pp. 1602--1625,  
  DOI: 10.4208/cicp.OA-2017-0012
